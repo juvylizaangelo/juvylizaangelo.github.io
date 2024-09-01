b96731d8baebfb793f2fd07f67f57cb1b5e5031b
juvylizaangelo <155311336+juvylizaangelo@users.noreply.github.com>
2024-09-01 19:20:11 +0100

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import img from '../assets/png/profileimg.png'
 
 export const headerData = {
     name: 'Juvyliza Angelo',
-    title: "Virtual Assistant",
+    title: "Looking for a new challenge | Production Engineer | Civil Engineer",
     desciption: "You create a gun module, a gun class, a foot module and a foot class. After realising you can't point the gun at the foot, you pass a reference to the gun to a foot object. After the foot is blown up, the gun object remains alive for eternity, ready to shoot all future feet that may happen to appear. [stolen from Mark] ",
     image: img,
     resumePdf: Juvyliza_Resume
