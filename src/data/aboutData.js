dc11a03a91d2500b1aac8d15fd2f5d37f7cade83
juvylizaangelo <155311336+juvylizaangelo@users.noreply.github.com>
2024-09-01 19:21:02 +0100

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is Juvyliza. I'm a Executive assistant based in Pangasinan, PH.",
+    description1: "My name is Juvyliza. I'm based in Pangasinan, PH.",
     description2: "I have 1.5 years of professional experience working as a Production Engineering (Senior) Staff at  Sumi North Philippines Wiring System (SNPW) - Binalonan, Philippines. I studied B.S. in Civil Engineering from Panpaciﬁc University. I am interested in digital assistant with project management responsibilities. I have many internship experiences working in management. Apart from that, I am also interested in designing using AutoCAD and other software based on adobe. I have passed Civil Service Exam (Professional level) in August 2023.",
     image: 1
 }
