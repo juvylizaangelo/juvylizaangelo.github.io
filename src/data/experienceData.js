89f77f13c6772a4ff7aa5b7efb60c5ed62163f75
juvylizaangelo <155311336+juvylizaangelo@users.noreply.github.com>
2024-10-16 09:00:41 +0100

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,11 +3,18 @@ export const experienceData = [
         id: 1,
         company: 'Sumi North Philippines Wiring System (SNPW) | Binalonan',
         jobtitle: ' Production Engineering (Senior) Staff',
+        startYear: 'Sept 2024 ',
+        endYear: ' Present'
+    },
+    {
+        id: 2,
+        company: 'Sumi North Philippines Wiring System (SNPW) | Binalonan',
+        jobtitle: ' Production Engineering (Senior) Staff',
         startYear: 'Dec 2022 ',
         endYear: ' April 2024'
     },
     {
-        id: 2,
+        id: 3,
         company: 'Linecs Engineering Solutions and Trading | Urdaneta',
         jobtitle: 'Design and Management Intern',
         startYear: 'June 2022 ',
