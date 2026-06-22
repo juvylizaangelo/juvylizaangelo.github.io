6d3ec6f558db2f851f6fb0e9e2be05577b434255
juvylizaangelo <155311336+juvylizaangelo@users.noreply.github.com>
2026-06-22 21:06:18 +0100

    Update servicesData.js

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -2,30 +2,29 @@
 import { BiShoppingBag, BiPencil } from "react-icons/bi";
 import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
 import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
-import { FaInternetExplorer, FaLaptop, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper, FaDraftingCompass } from "react-icons/fa";
-
+import { FaInternetExplorer, FaLaptop, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper, FaDraftingCompass, FaUsers } from "react-icons/fa";
 
 
 export const servicesData = [
     {
         id: 1,
         title: 'Virtual Assistance',
-        icon: <FaLaptop/>
+        icon: <AiOutlineMail />
     },
     {
         id: 2,
         title: 'Executive & Administrative Support',
-        icon: <AiOutlineMail/>
+        icon: <BsClipboardData />
     },
     {
         id: 3,
         title: 'Recruitment Operations',
-        icon: <BsClipboardData/>
+        icon: <FaUsers />
     },
     {
         id: 4,
         title: 'Operations & Project Coordination',
-        icon: <BsCodeSlash/>
+        icon: <FaRegNewspaper />
     }
 ]
 
